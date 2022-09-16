4118139282e3d4db36067a692bc758d8e9f2b833
Kyle Turman <kyle@turman.co>
2022-09-15 18:44:57 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Modool Example
-Here's how you can use (Modool)[https://github.com/kyleturman/modool] with a basic Webpack configuration.
+Here's how you can use [Modool](https://github.com/kyleturman/modool) with a basic Webpack configuration.
+
+Clone to your computer then run:
+```
+npm run start
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
-const path = require("path")
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      }
     ],
   },
-  mode: "development",
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  mode: 'development',
 }
